06f3f44ca118ae7994777c37a6a84f0097f249cd
StasyHard <reyngardt.as@yandex.ru>
2020-04-16 15:17:13 +0300

    Initial

---
+++ MYMovies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MYMovies
+//
+//  Created by Anastasia Reyngardt on 16.04.2020.
+//  Copyright © 2020 GermanyHome. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
